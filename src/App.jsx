7fe27dd80de123b260f3dc6dbfc8a64dbca5e2fc
yasmiinbianca <41231@raphaeldisanto.com.br>
2026-04-16 08:12:02 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <main>
+        <section>
+        <h1>TecFlow</h1>
+        <p>Your ultimate task manage companion</p>
+        <div class = "botoes">
+          <button>Discover</button>
+          <button>Login</button>
+        </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+
